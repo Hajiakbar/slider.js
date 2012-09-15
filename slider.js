5711b6a6951582e843d59e528d0cf686ffdbbf7d
Daniel Moffat <danmofo@gmail.com>
2012-09-15 13:29:22 +0100

    cleaned up code to pass jshint

--- slider.js
+++ slider.js
@@ -1,143 +1,134 @@
-// Constructor, set some initial values + fallbacks, fire off init, when called needs to be wrapped in $(function(){ //code }) otherwise it won't work
-var Slider = function(options) {
-    this.width = options.width || 880;
-    this.$el = options.el || $('body');
-    this.slideCount = options.slideCount || 4;
-    this.autoplay = options.autoPlay || false;
-    this.padding = options.padding || 50
-    this.totalWidth = (this.padding * 2) + this.width;
-    this.current = 1;
-    this.position = 0;
-    this.init();
-};  
+(function($) {
+    'use strict';
 
-// Adds the event handlers to the controls
-Slider.prototype.addEvents = function() {
-    var that = this; // So we can refer to Slider from within the event handler
-    this.$prev.bind('click', function() {
-        that.prev();
-    });
+    // Constructor, set some initial values + fallbacks, fire off init, when called needs to be wrapped in $(function(){ //code }) otherwise it won't work
+    var Slider = function(options) {
+        this.width = options.width || 880;
+        this.$el = options.el || $('body');
+        this.slideCount = this.$el.find('.slide').length;
+        this.autoplay = options.autoPlay || false;
+        this.current = 1;
+        this.position = 0;
+        this.init();
+    };  
 
-    this.$next.bind('click', function() {
-        if(that.current === 4) {
-            that.goto(1);   
+    // Creates all the jQuery variables for use throughout the widget, fires off the resize, addEvents and update methods
+    Slider.prototype.init = function() {
+        console.log('slider loaded');
+        this.$canvas = $('.canvas', this.$el);
+        this.$slides = $('.slide', this.$el);
+        this.$prev = $('.prev', this.$el);
+        this.$next = $('.next', this.$el);
+        this.$progress = $('.progress', this.$el);
+        this.resize();
+        this.addEvents();
+        this.update();
+        if(this.autoplay) {
+            this.autoPlay();
         }
-        else {
-            that.next();     
-        }
-    });
-};
-
-// Creates all the jQuery variables for use throughout the widget, fires off the resize, addEvents and update methods
-Slider.prototype.init = function() {
-    this.$canvas = $('.canvas', this.$el);
-    this.$slides = $('.slide', this.$el);
-    this.$prev = $('.prev', this.$el);
-    this.$next = $('.next', this.$el);
-    this.$progress = $('.progress', this.$el);
-    this.resize();
-    this.addEvents();
-    this.update();
-    if(this.autoplay) {
-        this.autoPlay();
-    }
-};
-
-Slider.prototype.goto = function(slide) {
-    slide - 1;
-    var that = this;
-    this.current = slide;
-    this.position = this.current * -this.totalWidth;
-    this.$canvas.animate({
-        'left' : this.position
-    }, 
-    'fast',
-    function() {
-        that.update();
-    });
-}
-
-// Ran when the next button is clicked
-Slider.prototype.next = function() {
-    var that = this;
-    if(this.current !== this.slideCount) {
-        this.position = this.position - this.totalWidth;
-        this.current++;
-        this.$canvas.animate({'left': this.position}, 'fast', function() {
-            that.update();
-        });
-    }
-};
-
-// Ran when the previous button is clicked
-Slider.prototype.prev = function() {
-    var that = this;
-    if(this.current !== 1){
-        this.position = this.position + this.totalWidth;
-        this.current--;
-        this.$canvas.animate({'left': this.position}, 'fast', function() {
-            that.update();
-        });
-    }
-};
-
-// Sets the dimensions of the slider, based on options passed to the constructor, this allows it to fit multiple widths rather than be a fixed width,
-// all you need to change are the dimensions you pass to new Slider()
-Slider.prototype.resize = function() {
-    this.$el.css('width', this.width + (this.padding * 2));
-    this.$canvas.css('width', this.totalWidth * this.slideCount)
-    this.$canvas.css('left', '0px');
-    this.$slides.css({
-        'padding': '0px ' + this.padding + 'px',
-        'width': this.width
-    });
-    this.$progress.css('left', (this.totalWidth - this.$progress.css('width').split('px').join('')) / 2);
-};
-
-// This method is called everytime a UI action takes place (prev, next, reset)
-Slider.prototype.update = function() {
-    console.log(this.current);
-    var nav = '';
-    switch(this.current) {
-        case 1:
-            nav = 'xooo'
-            break;
-        case 2:
-            nav = 'oxoo'
-            break;
-        case 3:
-            nav = 'ooxo'
-            break;
-        case 4:
-            nav = 'ooox'
-            break;
-        default:
-            nav = 'xooo'
     };
 
-    this.$progress.html(nav);
-};
+    // Adds the event handlers to the controls
+    Slider.prototype.addEvents = function() {
+        var that = this; // So we can refer to Slider from within the event handler
+        this.$prev.on('click', function() {
+            that.prev();
+        });
 
-// Auto plays the slider
-Slider.prototype.autoPlay = function() {
-    var that = this;
-    var play = setInterval(function() {
-        if(that.current !== 4) {
-            that.next();
-        }
-        else {
-            that.reset();
-        }
-    }, 5000);
-};
+        this.$next.on('click', function() {
+            if(that.current === that.slideCount) {
+                that.goto(1);   
+            }
+            else {
+                that.next();
+            }
+        });
+    };
 
-// Call the slider
-$(function(){
-    new Slider({
-        el: $('#slider'),
-        width: 800,
-        slideCount: 4,
-        padding: 40,
-        autoPlay: false
+    // Sets the dimensions of the slider + the slides
+    Slider.prototype.resize = function() {
+        this.$el.css('width', this.width);
+        this.$canvas.css('width', this.width * this.slideCount);
+        this.$canvas.css('left', '0px');
+        this.$slides.css({'width': this.width});
+    };
+
+    // Helper function to move you to a particular slide. TODO: use this for every slide (on next, prev etc..), as there is quite
+    // a bit of repeated code (for the animation)
+    Slider.prototype.goto = function(slide) {
+        var that = this;
+        this.current = slide;
+        if(slide === 1) {
+            this.position = 0;
+        } else {
+            this.position = this.current * -this.width;
+        }
+        this.$canvas.animate({'left' : this.position}, 'fast', that.update());
+    };
+
+    // Ran when the next button is clicked
+    Slider.prototype.next = function() {
+        var that = this;
+        if(this.current !== this.slideCount) {
+            this.position = this.position - this.width;
+            this.current++;
+            this.$canvas.animate({'left': this.position}, 'fast', that.update());
+        }
+    };
+
+    // Ran when the previous button is clicked
+    Slider.prototype.prev = function() {
+        var that = this;
+        if(this.current !== 1){
+            this.position = this.position + this.width;
+            this.current--;
+            this.$canvas.animate({'left': this.position}, 'fast', that.update());
+        }
+    };
+
+    // This method is called everytime a UI action takes place (prev, next, reset)
+    Slider.prototype.update = function() {
+        var nav = '';
+        switch(this.current) {
+            case 1:
+                nav = '<p>xoooo</p>';
+                break;
+            case 2:
+                nav = 'oxooo';
+                break;
+            case 3:
+                nav = 'ooxoo';
+                break;
+            case 4:
+                nav = 'oooxo';
+                break;
+            case 5:
+                nav = 'oooox';
+                break;
+        }
+        this.$progress.html(nav);
+    };
+
+    // Auto plays the slider
+    Slider.prototype.autoPlay = function() {
+        var that = this;
+        setInterval(function() {
+            if(that.current !== this.slideCount) {
+                that.next();
+            }
+            else {
+                that.goto(1);
+            }
+        }, 5000);
+    };
+
+    // Call the slider
+    $(function(){
+        new Slider({
+            el: $('#slider'),
+            width: 880,
+            autoPlay: false
+        });
     });
-});
+
+})(jQuery);
